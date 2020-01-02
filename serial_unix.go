a6a876ddb84268a47b18e16ba8de6cb8146f044d
Shaun <scooley@gmail.com>
2020-01-02 15:13:26 -0800

    Merging PR#63 ('Do not ignore errors on exclusive acquisition/release of the port') from the origin bugst repo https://github.com/bugst/go-serial/pull/63/files

--- serial_unix.go
+++ serial_unix.go
@@ -39,7 +39,7 @@ func (port *unixPort) Close() error {
 	}
 
 	// Close port
-	port.releaseExclusiveAccess()
+	_ = port.releaseExclusiveAccess()
 	if err := unix.Close(port.handle); err != nil {
 		return err
 	}
@@ -301,8 +301,12 @@ func nativeOpen(portName string, mode *Mode) (*unixPort, error) {
 
 	unix.SetNonblock(h, false)
 
-	port.acquireExclusiveAccess()
-
+	err = port.acquireExclusiveAccess()
+	if err != nil {
+		port.Close()
+		return nil, err
+	}
+	
 	// This pipe is used as a signal to cancel blocking Read
 	pipe := &unixutils.Pipe{}
 	if err := pipe.Open(); err != nil {
